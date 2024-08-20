7ce0af629b8c067e0ccab802eefbd48677a0637c
mohammedibrahimshawon <mohammedibrahimshawonaiub@gmail.com>
2024-08-20 21:56:14 +0600

    changes

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ const h1 = document.querySelector(".heading-primary");
 console.log(myName);
 console.log(h1);
 
-// h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
-//   h1.style.padding = "5rem";
-// });
+h1.addEventListener("click", function () {
+  h1.textContent = myName;
+  h1.style.backgroundColor = "red";
+  h1.style.padding = "5rem";
+});
 
 ///////////////////////////////////////////////////////////
 // Set current year
