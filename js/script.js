ddfad8c9a38823fd17c4bb664b8fcfe2d81a9d0f
mohammedibrahimshawon <mohammedibrahimshawonaiub@gmail.com>
2024-10-12 15:07:31 +0600

    nav bar function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
-console.log("Hello world!");
+// Functionality added to console log and modify heading on click
+function modifyHeading() {
+  console.log("Hello world!");
 
-const myName = "Jonas Schmedtmann";
-const h1 = document.querySelector(".heading-primary");
-console.log(myName);
-console.log(h1);
+  const myName = "Jonas Schmedtmann";
+  const h1 = document.querySelector(".heading-primary");
+  console.log(myName);
+  console.log(h1);
 
-h1.addEventListener("click", function () {
-  h1.textContent = myName;
-  h1.style.backgroundColor = "red";
-  h1.style.padding = "5rem";
-});
+  h1.addEventListener("click", function () {
+    h1.textContent = myName;
+    h1.style.backgroundColor = "red";
+    h1.style.padding = "5rem";
+  });
+}
+modifyHeading();
 
 ///////////////////////////////////////////////////////////
 // Set current year
@@ -27,46 +31,6 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-// Mobile navigation functionality
-// const btnMobileNav = document.querySelector('.btn-mobile-nav');
-// const navList = document.querySelector('.main-nav-list');
-
-// btnMobileNav.addEventListener('click', function () {
-//   navList.classList.toggle('nav-open');
-//   const menuIcon = document.querySelector('.icon-mobile-nav[name="menu-outline"]');
-//   const closeIcon = document.querySelector('.icon-mobile-nav[name="close-outline"]');
-
-//   menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none';
-//   closeIcon.style.display = closeIcon.style.display === 'block' ? 'none' : 'block';
-// });
-
-// const btnMobileNav = document.querySelector('.btn-mobile-nav');
-// const navList = document.querySelector('.main-nav-list');
-
-// // Toggle mobile navigation
-// btnMobileNav.addEventListener('click', function (event) {
-//   navList.classList.toggle('nav-open');
-//   const menuIcon = document.querySelector('.icon-mobile-nav[name="menu-outline"]');
-//   const closeIcon = document.querySelector('.icon-mobile-nav[name="close-outline"]');
-  
-//   // Toggle between menu and close icons
-//   menuIcon.style.display = menuIcon.style.display === 'none' ? 'block' : 'none';
-//   closeIcon.style.display = closeIcon.style.display === 'block' ? 'none' : 'block';
-// });
-
-// // Make sure no link behavior is blocked
-// document.querySelectorAll('.main-nav-link').forEach(link => {
-//   link.addEventListener('click', function(event) {
-//     // Remove event.preventDefault() to allow links to work normally
-//     if (navList.classList.contains('nav-open')) {
-//       navList.classList.remove('nav-open');
-//       document.querySelector('.icon-mobile-nav[name="menu-outline"]').style.display = 'block';
-//       document.querySelector('.icon-mobile-nav[name="close-outline"]').style.display = 'none';
-//     }
-//   });
-// });
-
-
 ///////////////////////////////////////////////////////////
 // Smooth scrolling animation
 
@@ -74,25 +38,28 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    e.preventDefault();
     const href = link.getAttribute("href");
 
     // Scroll back to top
-    if (href === "#")
+    if (href === "#") {
+      e.preventDefault();
       window.scrollTo({
         top: 0,
         behavior: "smooth",
       });
+    }
 
     // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
+      e.preventDefault();
       const sectionEl = document.querySelector(href);
       sectionEl.scrollIntoView({ behavior: "smooth" });
     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
       headerEl.classList.toggle("nav-open");
+    }
   });
 });
 
@@ -144,54 +111,3 @@ function checkFlexGap() {
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
-
-/*
-.no-flexbox-gap .main-nav-list li:not(:last-child) {
-  margin-right: 4.8rem;
-}
-
-.no-flexbox-gap .list-item:not(:last-child) {
-  margin-bottom: 1.6rem;
-}
-
-.no-flexbox-gap .list-icon:not(:last-child) {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .delivered-faces {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .meal-attribute:not(:last-child) {
-  margin-bottom: 2rem;
-}
-
-.no-flexbox-gap .meal-icon {
-  margin-right: 1.6rem;
-}
-
-.no-flexbox-gap .footer-row div:not(:last-child) {
-  margin-right: 6.4rem;
-}
-
-.no-flexbox-gap .social-links li:not(:last-child) {
-  margin-right: 2.4rem;
-}
-
-.no-flexbox-gap .footer-nav li:not(:last-child) {
-  margin-bottom: 2.4rem;
-}
-
-@media (max-width: 75em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 3.2rem;
-  }
-}
-
-@media (max-width: 59em) {
-  .no-flexbox-gap .main-nav-list li:not(:last-child) {
-    margin-right: 0;
-    margin-bottom: 4.8rem;
-  }
-}
-*/
